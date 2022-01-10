04bf51f2736479ea3bc3a3674b043a707c516f9b
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2022-01-10 07:56:23 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_089;
+
+namespace ServerChatApp_089_Prasetyo_Nur_Hidayat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallback));
+                hostObj.Open();
+
+                Console.WriteLine("Server On, Ready to Use");
+                Console.ReadLine();
+
+                hostObj.Close();
+            }
+            catch (Exception e)
+            {
+                hostObj = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
